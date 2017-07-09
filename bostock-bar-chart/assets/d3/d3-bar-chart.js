0fdd882feb3ad9ff5c56d291fdf2ba259128f8ef
tomcali <tmiller@toutbay.com>
2017-07-09 10:59:12 -0700

    vertical axis label added

--- bostock-bar-chart/assets/d3/d3-bar-chart.js
+++ bostock-bar-chart/assets/d3/d3-bar-chart.js
@@ -97,7 +97,14 @@ var data = [
       .call(d3.axisLeft(y));
 
 
-
+  // text label for the y axis
+  svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Investment Portfolio Percentage");      
 
 
 
